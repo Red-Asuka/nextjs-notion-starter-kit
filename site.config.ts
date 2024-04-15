254c5a1a24ad060a7e7702a9274005a3c4d92f40
Red-Asuka <liyd0623@gmail.com>
2024-04-15 14:27:59 +0800

    chore(config): update site config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,21 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Blog-Ethan-Lee-6179558ebe29414f934f74e8248ba61c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
-
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  name: 'Blog - Ethan Lee',
+  domain: 'https://crawling-friend-100.notion.site/Blog-Ethan-Lee-6179558ebe29414f934f74e8248ba61c',
+  author: 'Ethan Lee',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  github: 'Red-Asuka',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
